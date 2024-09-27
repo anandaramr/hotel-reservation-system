1a2260ffa175564264ce80939f7b3edf309aa4e2
Anandaram R <anandaramr1915@gmail.com>
2024-09-28 00:10:33 +0530

    Add authentication routes to server

---
+++ server/utils.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+function authorize(req,res,next) {
+    const token = req.headers.authorization?.split(" ")[1]
+    if(!token) return res.status(400).json({ error: "Token not found" });
+
+    jwt.verify(token, process.env.ACCESSKEY, (err, result) => {
+        if(err) return res.status(403).json({ error: err.message });
+
+        res.user = result
+        next()
+    })
+}
+
+module.exports = { authorize }
