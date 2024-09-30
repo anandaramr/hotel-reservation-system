63c378d163fe2c243bd22eca17538daec68c26db
Devika Krishnan M <dkrishnanm@gmail.com>
2024-09-30 22:24:49 +0530

    Modify DropDown

--- client/src/Components/DropDown.jsx
+++ client/src/Components/DropDown.jsx
@@ -1,14 +1,10 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 function DropDown()
 {
     const [counter,setCounter] = useState()
     const [room,setRoom] = useState('Single Room')
     const [ac,setAc] = useState('A/C')
-
-    useEffect(()=>{
-        console.log(counter)
-    },[counter])
 
     const roomOptions = [
         {text:'Single Room',val:'single'},
@@ -34,40 +30,38 @@ function DropDown()
         setCounter(0)
         setAc(ac.text)
     }
-
+    
     return(
         <>
-        <div className="border-2 border-white p-2 rounded-lg select-none text-lg grid grid-cols-3">
-                <div className="col-start-1">
-                    <div onClick={()=>(counter !=1 && setCounter(1))} className={`min-w-[30%] bg-orange-100 flex flex-row items-center justify-center py-2 col-start-1 ${counter==1 && 'absolute'}`}>
-                        <div className="flex flex-col">
-                            <div className="flex flex-row px-4 py-2 group">
-                                <p className="text-center w-96 text-gray-800">{room}</p>
-                                <span className="material-symbols-outlined text-rose-900 group-hover:text-rose-600">keyboard_double_arrow_down</span>
-                            </div>
-                            <div className={`text-center bg-white ${counter!=1 && 'hidden'}`}>
-                                {roomOptions.map((room)=><div onClick={()=>clickRoom(room)} className="hover:bg-orange-200" key={room.val}>{room.text}</div>)}
-                            </div>
-                        </div>
+            <div className="border-2 border-white grid grid-cols-5 p-3 text-xl gap-3 rounded-md h-[10svh] cursor-default select-none">
+
+                {/* Dropdown 1 */}
+                <div className="col-start-1 col-span-2 ">
+                    <div onClick={()=> setCounter(1)} className="col-start-1 col-span-2 flex flex-row rounded-lg justify-between bg-orange-100 items-center p-2 w-96">
+                        <p className="text-center w-full">{room}</p>
+                        <span className="material-symbols-outlined text-rose-900 group-hover:text-rose-600">keyboard_double_arrow_down</span>
                     </div>
-                </div> 
-                <div className="col-start-2">
-                    <div onClick={()=>counter !=2 && setCounter(2)} className={` min-w-[30%] bg-orange-100 flex flex-row items-center justify-center py-2  ${counter==2 && 'absolute'}`}>
-                        <div className="flex flex-col">
-                            <div className="flex flex-row px-4 py-2 group">
-                                <p className="text-center w-96 text-gray-800 hover:text-black">{ac}</p>
-                                <span className="material-symbols-outlined text-rose-900 group-hover:text-rose-600">keyboard_double_arrow_down</span>
-                            </div>
-                            <div className={`${counter!=2 && 'hidden'} text-center bg-white`}>
-                            {acOptions.map((ac)=><div onClick={()=>clickAc(ac)} className="hover:bg-orange-200" key={ac.val}>{ac.text}</div>)}
-                            </div>
-                        </div>
+                    <div  className={` ${counter !=1 && 'hidden'} rounded-lg my-3 bg-orange-100 py-2`}>
+                        {roomOptions.map((item)=><div className="text-center hover:bg-orange-200 my-1" onClick={()=>clickRoom(item)} key={item.val}>{item.text}</div>)}
                     </div>
                 </div>
-                <div className="flex justify-center items-center">
-                    <button className="text-xl px-10 py-3 w-fit h-fit rounded-lg bg-orange-950 text-white hover:bg-white hover:text-orange-950">Search</button>
+                
+
+                {/* Dropdown 2 */}
+                <div className="col-start-3 col-span-2">
+                    <div onClick={()=> setCounter(2)} className="col-start-3 col-span-2 rounded-lg flex flex-row justify-between bg-orange-100 items-center p-2 w-96">
+                        <p className="text-center w-full">{ac}</p>
+                        <span className="material-symbols-outlined text-rose-900 group-hover:text-rose-600">keyboard_double_arrow_down</span>
+                    </div>
+                    <div className={` ${counter !=2 && 'hidden'} rounded-lg my-3 bg-orange-100 py-2`}>
+                        {acOptions.map((item)=><div className="text-center hover:bg-orange-200 my-1" onClick={()=>clickAc(item)}  key={item.val}>{item.text}</div>)}
+                    </div>
                 </div>
-        </div>
+                {/* Search */}
+                <div className="col-start-5 col-span-1 flex justify-center h-fit">
+                    <button className="rounded-xl hover:bg-rose-900 text-white px-7 py-2 duration-150 bg-rose-950">Search</button>
+                </div>
+            </div>
         </>
     )
 }
