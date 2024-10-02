08d7b475befd975a8ad0c5e184cc20703a10a24f
Ananthakrishnan J <2004ananthu@gmail.com>
2024-10-02 22:49:46 +0530

    Create menu page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Signup from "./Pages/Signup"
 import Login from './Pages/Login'
 import Home from "./Pages/Home"
 import Profile from "./Pages/Profile"
+import Menu from "./Pages/Menu"
 import  { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { useEffect } from "react"
 
@@ -18,6 +19,8 @@ function App() {
           <Route path="/signup" element = {<Signup/>} />
           <Route path="/login" element = {<Login/>} />
           <Route path="/profile" element= {<Profile/>} />
+          <Route path="/menu" element= {<Menu/>} />
+
       </Routes>
     </BrowserRouter>
   )
